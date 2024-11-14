fea11569f33acc41099e5a415e6eeba76fbd7cb1
Taylor Dawson <taylorjdawson@gmail.com>
2024-11-13 21:46:41 -0800

    Fix env vars

--- apps/validator-dashboard/src/config/mev-commit.ts
+++ apps/validator-dashboard/src/config/mev-commit.ts
@@ -7,16 +7,7 @@ const chain = getChain()
 
 export const mevCommitConfig: MevCommitConfig = {
   chain,
-  vanillaRegistryAddress:
-    chain === "holesky"
-      ? env.NEXT_PUBLIC_VANILLA_REGISTRY_ADDRESS_HOLESKY
-      : env.NEXT_PUBLIC_VANILLA_REGISTRY_ADDRESS,
-  middlewareAddress:
-    chain === "holesky"
-      ? env.NEXT_PUBLIC_MEV_COMMIT_MIDDLEWARE_ADDRESS_HOLESKY
-      : env.NEXT_PUBLIC_MEV_COMMIT_MIDDLEWARE_ADDRESS,
-  avsV3Address:
-    chain === "holesky"
-      ? env.NEXT_PUBLIC_MEV_COMMIT_AVS_V3_ADDRESS_HOLESKY
-      : env.NEXT_PUBLIC_MEV_COMMIT_AVS_V3_ADDRESS,
+  vanillaRegistryAddress: env.NEXT_PUBLIC_VANILLA_REGISTRY_ADDRESS,
+  middlewareAddress: env.NEXT_PUBLIC_MEV_COMMIT_MIDDLEWARE_ADDRESS,
+  avsV3Address: env.NEXT_PUBLIC_MEV_COMMIT_AVS_V3_ADDRESS,
 }
